336a447cfd2ca5b6376dc989bc409a5d46541378
clys-man <clysmanalves@gmail.com>
2024-06-02 00:05:39 -0300

    feat: add acl

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return [
+        'app' => config('app.name')
+    ];
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
